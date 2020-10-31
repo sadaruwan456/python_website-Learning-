4681de169d2cab44e4839db5d5dbfe831a73cd10
sadaruwan456 <60360437+sadaruwan456@users.noreply.github.com>
2020-10-31 05:50:48 -0700

    First Commit

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\user\pycharmprojects\python-web-test\pythonweb\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
